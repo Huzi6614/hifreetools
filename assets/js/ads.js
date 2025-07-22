4e110a21006703c6ce476d21515719b43874b45c
Huzi6614 <huzaifasuleman300@gmail.com>
2025-07-22 17:05:33 +0500

    Update ads.js

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -1,1 +1,8 @@
-<script src="https://fpyf8.com/88/tag.min.js" data-zone="158630" async data-cfasync="false"></script>
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://fpyf8.com/88/tag.min.js';
+  script.setAttribute('data-zone', '158630');
+  script.async = true;
+  script.setAttribute('data-cfasync', 'false');
+  document.body.appendChild(script);
+})();
