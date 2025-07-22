7618beb9898c58866b362b063caba46852d5c6a8
Huzi6614 <huzaifasuleman300@gmail.com>
2025-07-22 17:18:51 +0500

    Update ads.js

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -1,8 +1,8 @@
 (function() {
   var script = document.createElement('script');
-  script.src = 'https://fpyf8.com/88/tag.min.js';
+  script.setAttribute('data-cfasync', 'false');
   script.setAttribute('data-zone', '158630');
   script.async = true;
-  script.setAttribute('data-cfasync', 'false');
-  document.body.appendChild(script);
+  script.src = 'https://fpyf8.com/88/tag.min.js';
+  (document.head || document.body).appendChild(script);
 })();
